62a6aefa3799a9777894a1aaa015a64e86ede872
Lenka <aruru.goddes@gmail.com>
2024-06-08 23:54:38 +0300

    Выполнена домашняя работа № 3.2 "Введение в базы данных"

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -3,56 +3,50 @@ package ru.hogwarts.school.service;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.EntityNotFoundException;
 import ru.hogwarts.school.model.Faculty;
-import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
-    private final Map<Long, Faculty> faculties = new HashMap<>();
-    private static Long idCounter = 1L;
+    private final FacultyRepository facultyRepository;
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
     @Override
     public Faculty add(Faculty faculty) {
-        faculties.put(idCounter++, faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty get(Long id) {
-        if (faculties.containsKey(id)) {
-            return faculties.get(id);
-        }
-        throw new EntityNotFoundException();
+        return facultyRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
     public Faculty update(Faculty faculty) {
-        if (faculties.containsKey(faculty.getId())) {
-            return faculties.put(faculty.getId(), faculty);
-        }
-        throw new EntityNotFoundException();
+        return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty remove(Long id) {
-        if (faculties.containsKey(id)) {
-            return faculties.remove(id);
-        }
-        throw new EntityNotFoundException();
+        Faculty faculty = get(id);
+        facultyRepository.deleteById(id);
+        return faculty;
     }
 
     @Override
     public Collection<Faculty> getByColor(String color) {
-        return faculties.values().stream()
+        return getAll().stream()
                 .filter(s -> s.getColor().equals(color))
                 .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Faculty> getAll() {
-        return faculties.values();
+        return facultyRepository.findAll();
     }
 }
